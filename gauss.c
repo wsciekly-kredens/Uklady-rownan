1f6d9184ed50c6bfce5c1e6c16fb787708e01a63
Marcin Kowalski <01168577@pw.edu.pl>
2021-12-15 18:46:48 +0100

    Właściwa struktura programu i poprawna wersja pliku gauss.c

---
+++ gauss.c
@@ -0,0 +1,27 @@
+#include "gauss.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ */
+int eliminate(Matrix *mat, Matrix *b){
+	int r = mat->r;
+	int c = mat->c;
+	if(r<=0||c<=0)
+		return -1;
+	for(int i = 0; i<c-1;i++){
+		for(int j = 0; j<r; j++){
+			if( mat->data[i][i] < 0.0000001)
+				return 1;
+			double q = mat->data[j][i]/mat->data[i][i];
+			for(int k = 1; k<r;k++){
+				mat->data[j][k] -= q * mat->data[k][k];
+			}
+			b->data[j][0] -= q*b->data[i][0];
+		}
+	}
+	return 0;
+}
+
